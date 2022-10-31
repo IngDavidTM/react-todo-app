cddef74bb117452020a0956509cf3639863c7eb1
IngDavidTM <david-1011@hotmail.com>
2022-10-31 12:54:08 -0500

    update onchange method

--- src/components/InputTodo.js
+++ src/components/InputTodo.js
@@ -1,10 +1,26 @@
 import React, { Component } from 'react';
 
 class InputTodo extends Component {
+  state = {
+    title: ""
+  };
+
+  onChange = (e) => {
+    this.setState({
+      [e.target.name]: e.target.value,
+    });
+  };
+
   render() {
     return (
       <form>
-        <input type='text' placeholder='Add Todo...' />
+        <input
+          type="text"
+          placeholder="Add todo..."
+          value={this.state.title}
+          name="title"
+          onChange={this.onChange}
+        />
         <button>Submit</button>
       </form>
     )
