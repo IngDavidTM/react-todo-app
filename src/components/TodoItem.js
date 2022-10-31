ffaf97984753ae620d9ab532489fedc596d43324
IngDavidTM <david-1011@hotmail.com>
2022-10-31 15:50:44 -0500

    add localstorage

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -10,6 +10,10 @@ class TodoItem extends React.Component {
     };
   }
 
+  componentWillUnmount() {
+    console.log("Cleaning up...")
+  }
+
   handleEditing = () => {
     this.setState({
       editing: true,
