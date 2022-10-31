8ead22ded0412c69c38c34fe11fce237743a2df7
IngDavidTM <david-1011@hotmail.com>
2022-10-31 10:32:46 -0500

    setup linters

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
-import React from "react"
-import ReactDOM from "react-dom"
-//component file
-import TodoContainer from "./components/TodoContainer"
-ReactDOM.render(<TodoContainer />, document.getElementById("root"))
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// component file
+
+import TodoContainer from './components/TodoContainer';
+
+ReactDOM.render(<TodoContainer />, document.getElementById('root'));
